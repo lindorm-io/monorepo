961222ce2ff11e8baea5df30cdbfb142f6a22e1c
Jonn Nilsson <jonn@jonn.nu>
2022-04-25 08:21:03 +0200

    feat: add base64url encoding to core package

--- packages/core/src/util/base64.ts
+++ packages/core/src/util/base64.ts
@@ -5,3 +5,17 @@ export const baseHash = (input: string): string => {
 export const baseParse = (input: string): string => {
   return Buffer.from(input, "base64").toString("utf8");
 };
+
+const fromBase64 = (base64: string): string =>
+  base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+
+export const encodeBase64Url = (input: any, encoding: BufferEncoding = "utf8"): string => {
+  if (Buffer.isEncoding("base64url")) {
+    return Buffer.from(input, encoding).toString("base64url");
+  } else {
+    return fromBase64(Buffer.from(input, encoding).toString("base64"));
+  }
+};
+
+export const decodeBase64Url = (input: string): string =>
+  Buffer.from(input, "base64").toString("utf8");
