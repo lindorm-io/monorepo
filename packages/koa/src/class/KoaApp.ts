688beb1e9565104041d82bfa74097dd6b842a84b
Jonn Nilsson <jonn@jonn.nu>
2022-05-16 07:26:57 +0200

    fix: add verbose logging when attaching socket io

--- packages/koa/src/class/KoaApp.ts
+++ packages/koa/src/class/KoaApp.ts
@@ -76,6 +76,7 @@ export class KoaApp<Context extends DefaultLindormKoaContext = DefaultLindormKoa
     ];
 
     if (options.socket) {
+      this.logger.verbose("attaching socket.io server");
       this.ioServer = new IOServer(this.httpServer);
       this.middleware.push(socketIoMiddleware(this.ioServer));
 
