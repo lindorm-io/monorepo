37fe6c45df5be270bcfd30ba2e1eac3ee178a95c
Jonn Nilsson <jonn@jonn.nu>
2022-06-01 11:11:05 +0200

    fix: update oauth service bootstrap script

--- services/oauth-service/src/bin/bootstrap-dev.ts
+++ services/oauth-service/src/bin/bootstrap-dev.ts
@@ -9,17 +9,17 @@ const ids = {
   communication: "aec0a293-4371-450a-bcfa-0f60f1f51a65",
   device: "8da48653-ba29-416e-b882-393b60dce178",
   identity: "9993fa84-bedf-4a93-a421-1f63719cd9d3",
+  oidc: "3e0ab7aa-f445-4031-ad27-d87ff23d2ce2",
+  vault: "c233687a-c610-412c-985b-5f6e95fbf47d",
 };
 
 const secrets = {
-  authentication:
-    "JnywQD4xI1r0TtZNHSXpXFiZQbknHRGMpMGuPsILR3OYTtQDEUrb5KtQzu8BhbWhYYjtew49yN3VsFa38SkWDZ4CxgcOtfj6MNiCmFBRdzfAOkk4fJS1he11ViDEYTXp",
-  communication:
-    "23mxtxcwb5EcPcBCR43YN8bwnw1axbswKkZjam1qDrSO77NOR90s25pekjEbuzEIVuLzy8jeFm5781Xbt2U5btKnCLhOtddp4jI5JzfHghxqbkWDEU1aCKFzAQdC1OFT",
-  device:
-    "X4P2a8llWDVJbtzRAVnHObcLyOMAe2vYa2GAs2kSNXonlRcWblDYU07bJxC20B8Co2PJKpTITUBs2hEOV90Vr4o5zKhYme067RortdPmGoVpa6neHqNDZGIF1YXVcp36",
-  identity:
-    "bAJL5tCZr5lWBmJieQKqyFN9mBf8qN1G00YlIlTMtURg3gxJyFUSHvNvCTje3rdXNTifCX4xZHXeX8hvcWjgpmaZv9dS79OX2wK25XCl3NCI7DTwHIrBzL1zzh6A2uOb",
+  authentication: "kHuCFwhRR3joU3sjMPiKC0P5XwB62AAoYHJ6H9T6jJcYcpm3Vnj9AyYDIJGttD1E",
+  communication: "8uXj4edcXTOvT7BbB9ecspvfXrujPmIViFr7jw2k5sZVlleDI58WUg6V6HbjG2Md",
+  device: "siKP6qVeEoBxTOPV4Q4Ijiy6laUhgKXaZfrSYWJKBm3IfiO04fie7KIeFJdIbFUJ",
+  identity: "SXYF5eM47qn2fpqLkaeoy6c1R0AHKPssz0TfCpM3evxJzNasfrgaeFeMByJLmhpq",
+  oidc: "5NTac6bAnSNAWnMh0pCjABmlv1ca6LUml88gaQrQrBU3aNS6W6LpJp9r3ViclTe8",
+  vault: "aOpwpLy8XQhxExub2h68K0Puddy5CM9FO1v1XSZVIhvoSRvGlv0Fih6ofUCrwNmx",
 };
 
 const repositories = {
@@ -127,11 +127,55 @@ const main = async (): Promise<void> => {
   );
   await caches.client.create(identity);
 
+  const oidc = await repositories.client.create(
+    new Client({
+      id: ids.oidc,
+      active: true,
+      allowed: {
+        grantTypes: [GrantType.CLIENT_CREDENTIALS],
+        responseTypes: [],
+        scopes: Object.values(ClientScope),
+      },
+      host: "http://localhost",
+      name: "lindorm.io/oidc-service",
+      permissions: Object.values(ClientPermission),
+      redirectUris: [],
+      logoutUri: "http://localhost/logout",
+      secret: await argon.encrypt(secrets.oidc),
+      tenant: tenant.id,
+      type: ClientType.CONFIDENTIAL,
+    }),
+  );
+  await caches.client.create(oidc);
+
+  const vault = await repositories.client.create(
+    new Client({
+      id: ids.vault,
+      active: true,
+      allowed: {
+        grantTypes: [GrantType.CLIENT_CREDENTIALS],
+        responseTypes: [],
+        scopes: Object.values(ClientScope),
+      },
+      host: "http://localhost",
+      name: "lindorm.io/vault-service",
+      permissions: Object.values(ClientPermission),
+      redirectUris: [],
+      logoutUri: "http://localhost/logout",
+      secret: await argon.encrypt(secrets.vault),
+      tenant: tenant.id,
+      type: ClientType.CONFIDENTIAL,
+    }),
+  );
+  await caches.client.create(vault);
+
   logger.info("Generated Entities", {
     authentication: { id: ids.authentication, secret: secrets.authentication },
     communication: { id: ids.communication, secret: secrets.communication },
     device: { id: ids.device, secret: secrets.device },
     identity: { id: ids.identity, secret: secrets.identity },
+    oidc: { id: ids.oidc, secret: secrets.oidc },
+    vault: { id: ids.vault, secret: secrets.vault },
     tenant: { id: tenant.id, owner: tenant.owner },
   });
 };
