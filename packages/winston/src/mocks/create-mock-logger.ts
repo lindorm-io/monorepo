adc4a5fa164157efc71bf39baca276dc3d0f6783
Jonn Nilsson <jonn@jonn.nu>
2022-06-03 22:04:25 +0200

    fix: remove unnecessary implementations from mock logger

--- packages/winston/src/mocks/create-mock-logger.ts
+++ packages/winston/src/mocks/create-mock-logger.ts
@@ -1,33 +1,21 @@
-import { FilterCallback, LogDetails, LoggerTransportOptions, SessionMetadata } from "../types";
 import { ILogger } from "../types";
-import { LogLevel } from "../enum";
 
 export const createMockLogger = (): ILogger => ({
-  error: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
-  warn: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
-  info: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
-  verbose: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
-  debug: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
-  silly: jest.fn().mockImplementation((message: string, details?: LogDetails): void => undefined),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  verbose: jest.fn(),
+  debug: jest.fn(),
+  silly: jest.fn(),
 
-  createChildLogger: jest
-    .fn()
-    .mockImplementation((context: Array<string>): ILogger => createMockLogger()),
-  createSessionLogger: jest
-    .fn()
-    .mockImplementation((session: SessionMetadata): ILogger => createMockLogger()),
+  createChildLogger: jest.fn().mockImplementation((): ILogger => createMockLogger()),
+  createSessionLogger: jest.fn().mockImplementation((): ILogger => createMockLogger()),
 
-  addConsole: jest
-    .fn()
-    .mockImplementation(
-      (level: LogLevel, options: Partial<LoggerTransportOptions>): void => undefined,
-    ),
-  addContext: jest.fn().mockImplementation((context: Array<string>): void => undefined),
-  addSession: jest.fn().mockImplementation((session: Record<string, any>): void => undefined),
+  addConsole: jest.fn(),
+  addContext: jest.fn(),
+  addSession: jest.fn(),
 
-  setFilter: jest
-    .fn()
-    .mockImplementation((path: string, callback?: FilterCallback): void => undefined),
-  deleteFilter: jest.fn().mockImplementation((path: string): void => undefined),
-  clearFilters: jest.fn().mockImplementation((): void => undefined),
+  setFilter: jest.fn(),
+  deleteFilter: jest.fn(),
+  clearFilters: jest.fn(),
 });
