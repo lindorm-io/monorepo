fd6abe29350a9b2e178f700224d9cfbbc65f037a
Jonn Nilsson <jonn@jonn.nu>
2022-06-12 08:30:07 +0200

    fix: add server error when allowed flows are missing

--- services/authentication-service/src/util/get-prioritized-flow.ts
+++ services/authentication-service/src/util/get-prioritized-flow.ts
@@ -2,9 +2,16 @@ import { LoginSession } from "../entity";
 import { FlowType } from "../enum";
 import { filter, includes, orderBy } from "lodash";
 import { FLOW_TYPE_CONFIG } from "../constant";
+import { ServerError } from "@lindorm-io/errors";
 
 export const getPrioritizedFlow = (loginSession: LoginSession): FlowType => {
   const { allowedFlows } = loginSession;
+
+  if (!allowedFlows.length) {
+    throw new ServerError("Unexpected Error", {
+      description: "Login Session has no allowed flows",
+    });
+  }
 
   const config = filter(FLOW_TYPE_CONFIG, (item) => includes(allowedFlows, item.name));
 
