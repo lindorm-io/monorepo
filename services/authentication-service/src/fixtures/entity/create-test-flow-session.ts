1bf08caaefa92d695b17b2b67be501c2d7542e83
Jonn Nilsson <jonn@jonn.nu>
2022-06-12 08:29:22 +0200

    test: improve fixtures

--- services/authentication-service/src/fixtures/entity/create-test-flow-session.ts
+++ services/authentication-service/src/fixtures/entity/create-test-flow-session.ts
@@ -1,18 +1,19 @@
 import { FlowSession, FlowSessionOptions } from "../../entity";
 import { SessionStatus } from "../../common";
-import { baseHash } from "@lindorm-io/core";
+import { baseHash, getRandomNumber, getRandomString } from "@lindorm-io/core";
 import { FlowType } from "../../enum";
+import { randomUUID } from "crypto";
 
 export const createTestFlowSession = (options: Partial<FlowSessionOptions> = {}): FlowSession =>
   new FlowSession({
-    code: "7598650d",
+    code: baseHash(getRandomString(64)),
     email: "test@lindorm.io",
     expires: new Date("2022-01-01T08:00:00.000Z"),
-    loginSessionId: "b1c842ec-e7b2-4f88-a2b4-1385922bd232",
+    loginSessionId: randomUUID(),
     nin: "19010101-9999",
-    nonce: "4369b02b0b4f4d2d",
-    otp: baseHash("123456"),
-    phoneNumber: "0703557799",
+    nonce: getRandomString(16),
+    otp: baseHash(getRandomNumber(6).toString()),
+    phoneNumber: `07${getRandomNumber(8)}`,
     status: SessionStatus.PENDING,
     type: FlowType.EMAIL_OTP,
     username: "username",
--- services/authentication-service/src/fixtures/entity/create-test-login-session.ts
+++ services/authentication-service/src/fixtures/entity/create-test-login-session.ts
@@ -1,23 +1,35 @@
 import { LoginSession, LoginSessionOptions } from "../../entity";
 import { PKCEMethod } from "@lindorm-io/core";
+import { randomUUID } from "crypto";
+import { FlowType } from "../../enum";
 
 export const createTestLoginSession = (options: Partial<LoginSessionOptions> = {}): LoginSession =>
   new LoginSession({
-    allowedFlows: [],
-    allowedOidc: [],
+    allowedFlows: [
+      FlowType.BANK_ID_SE,
+      FlowType.DEVICE_CHALLENGE,
+      FlowType.EMAIL_LINK,
+      FlowType.EMAIL_OTP,
+      FlowType.PASSWORD,
+      FlowType.PHONE_OTP,
+      FlowType.RDC_QR_CODE,
+      FlowType.SESSION_ACCEPT_WITH_CODE,
+      FlowType.WEBAUTHN,
+    ],
+    allowedOidc: ["apple", "google", "microsoft"],
     amrValues: [],
     country: "se",
-    deviceLinks: ["b43722f5-34c4-4f2e-befb-c59f87a9fa03"],
-    expires: new Date("2022-01-01T08:00.00.000Z"),
-    identityId: "f9162629-26bf-453e-b6e6-42182d9da16f",
+    deviceLinks: [randomUUID()],
+    expires: new Date("2022-01-01T08:00:00.000Z"),
+    identityId: randomUUID(),
     levelOfAssurance: 0,
     loginHint: ["test@lindorm.io", "+46705498721"],
-    oauthSessionId: "975b54ff-7c4d-42fc-87dd-5f36541df9a9",
+    oauthSessionId: randomUUID(),
     pkceChallenge: "c0a0aaeaf54b4a58bb918411a33b7c24",
     pkceMethod: PKCEMethod.S256,
     remember: true,
     requestedAuthenticationMethods: ["email_otp"],
     requestedLevelOfAssurance: 2,
-    sessions: ["765ac486-e72c-49c3-bd01-ddfd5360e24b"],
+    sessions: [randomUUID(), randomUUID()],
     ...options,
   });
