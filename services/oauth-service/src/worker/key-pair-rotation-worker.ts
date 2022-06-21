c538dbf876495d655c87396fc25286227c9c8e4c
Jonn Nilsson <jonn@jonn.nu>
2022-06-21 07:34:32 +0200

    fix: add origin to services

--- services/oauth-service/src/worker/key-pair-rotation-worker.ts
+++ services/oauth-service/src/worker/key-pair-rotation-worker.ts
@@ -1,8 +1,10 @@
+import { configuration } from "../server/configuration";
 import { keyPairRotationWorker as _keyPairRotationWorker } from "@lindorm-io/koa-keystore";
 import { mongoConnection } from "../instance";
 import { winston } from "../server/logger";
 
 export const keyPairRotationWorker = _keyPairRotationWorker({
   mongoConnection,
+  origin: configuration.server.issuer,
   winston,
 });
--- services/vault-service/src/worker/key-pair-rotation-worker.ts
+++ services/vault-service/src/worker/key-pair-rotation-worker.ts
@@ -1,16 +1,19 @@
+import { KeyType } from "@lindorm-io/key-pair";
+import { configuration } from "../server/configuration";
 import { keyPairRotationWorker } from "@lindorm-io/koa-keystore";
 import { mongoConnection } from "../instance";
 import { winston } from "../server/logger";
-import { KeyType } from "@lindorm-io/key-pair";
 
 export const keyPairEcRotationWorker = keyPairRotationWorker({
   keyType: KeyType.EC,
   mongoConnection,
+  origin: configuration.server.issuer,
   winston,
 });
 
 export const keyPairRsaRotationWorker = keyPairRotationWorker({
   keyType: KeyType.RSA,
   mongoConnection,
+  origin: configuration.server.issuer,
   winston,
 });
