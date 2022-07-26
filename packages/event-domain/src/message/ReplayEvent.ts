22b01d9d4000e50df725238fa44656a4d32678ae
Jonn Nilsson <jonn@jonn.nu>
2022-07-26 16:36:11 +0200

    fix: amend error in replay event message

--- packages/event-domain/src/message/ReplayEvent.ts
+++ packages/event-domain/src/message/ReplayEvent.ts
@@ -1,12 +1,13 @@
-import { IMessage, ReplayEventData, MessageOptions } from "../types";
+import { IMessage, MessageOptions, Data } from "../types";
 import { Message } from "./Message";
 import { MessageType } from "../enum";
 
-export class ReplayEvent extends Message<ReplayEventData> implements IMessage {
-  public constructor(options: MessageOptions<ReplayEventData>, causation?: Message) {
+export class ReplayEvent<D extends Data = Data> extends Message<D> implements IMessage {
+  public constructor(options: MessageOptions<D>, causation?: Message) {
     super(
       {
         ...options,
+        mandatory: true,
         type: MessageType.REPLAY_EVENT,
       },
       causation,
