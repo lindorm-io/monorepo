c826babc588c5f3b8a4f1319a4c37000b75bf3fd
Jonn Nilsson <jonn@jonn.nu>
2022-07-26 16:36:54 +0200

    fix: add new replay event name

--- packages/event-domain/src/enum/ReplayEventName.ts
+++ packages/event-domain/src/enum/ReplayEventName.ts
@@ -1,5 +1,6 @@
 export enum ReplayEventName {
-  DROP_VIEWS = "replay_drop_views",
+  DROP_VIEW = "replay_drop_view",
+  MOVE_VIEW = "replay_move_view",
   PUBLISH_EVENTS = "replay_publish_events",
   START = "replay_start",
   STOP = "replay_stop",
