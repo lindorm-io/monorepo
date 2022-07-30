43b714ee230285d600b46f3f85cd0cab0c28fa52
Jonn Nilsson <jonn@jonn.nu>
2022-07-30 20:51:37 +0200

    fix: add typing to stringify blob function

--- packages/string-blob/src/util/stringify-blob.ts
+++ packages/string-blob/src/util/stringify-blob.ts
@@ -1,7 +1,7 @@
 import { getMetaObject } from "./get-meta-object";
 import { stringifyObjectValues } from "./stringify-object-values";
 
-export const stringifyBlob = (object: Record<string, any>): string => {
+export const stringifyBlob = <T = Record<string, any>>(object: T): string => {
   return JSON.stringify({
     json: stringifyObjectValues(object),
     meta: getMetaObject(object),
