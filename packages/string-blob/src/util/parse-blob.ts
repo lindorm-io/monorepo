353a3b0e6767cd76284ad1de6b263057f8efd332
Jonn Nilsson <jonn@jonn.nu>
2022-08-30 22:31:17 +0200

    fix: handle arrays in string-blob

--- packages/string-blob/src/util/parse-blob.ts
+++ packages/string-blob/src/util/parse-blob.ts
@@ -1,7 +1,11 @@
+import { isArray, isString } from "lodash";
+import { parseArrayValue } from "./parse-array-value";
 import { parseObjectValue } from "./parse-object-value";
 
-export const parseBlob = <T = Record<string, any>>(string: string): T => {
-  const { json, meta } = JSON.parse(string);
+export const parseBlob = <T = Record<string, any>>(input: any): T => {
+  const { json, meta } = JSON.parse(isString(input) ? input : JSON.stringify(input));
 
-  return parseObjectValue(json, meta) as T;
+  return isArray(json)
+    ? (parseArrayValue(json, meta) as unknown as T)
+    : (parseObjectValue(json, meta) as unknown as T);
 };
--- packages/string-blob/src/util/stringify-blob.ts
+++ packages/string-blob/src/util/stringify-blob.ts
@@ -1,9 +1,12 @@
+import { getMetaArray } from "./get-meta-array";
 import { getMetaObject } from "./get-meta-object";
+import { isArray } from "lodash";
+import { stringifyArrayValues } from "./stringify-array-values";
 import { stringifyObjectValues } from "./stringify-object-values";
 
-export const stringifyBlob = <T = Record<string, any>>(object: T): string => {
+export const stringifyBlob = <T = Record<string, any>>(input: T): string => {
   return JSON.stringify({
-    json: stringifyObjectValues(object),
-    meta: getMetaObject(object),
+    json: isArray(input) ? stringifyArrayValues(input) : stringifyObjectValues(input),
+    meta: isArray(input) ? getMetaArray(input) : getMetaObject(input),
   });
 };
