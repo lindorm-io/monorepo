fb21f122c26ab14b23e955c9c676945f424d67c8
Jonn Nilsson <jonn@jonn.nu>
2022-12-10 10:30:41 +0100

    fix: improve storage hook

--- applications/auth-web/src/hooks/use-storage.ts
+++ applications/auth-web/src/hooks/use-storage.ts
@@ -1,48 +1,32 @@
-type Storage = "local" | "session";
+export type Storage = "local" | "session";
 type StorageType = "localStorage" | "sessionStorage";
+
+type UseStorageHook = {
+  getItem: <T = unknown>(key: string, defaultValue?: T) => T | undefined;
+  removeItem: (key: string) => void;
+  setItem: <T = unknown>(key: string, value: T) => void;
+};
 
 const isBrowser = (): boolean => typeof window !== "undefined";
 
 const getStorageType = (type?: Storage): StorageType =>
   type === "session" ? "sessionStorage" : "localStorage";
 
-type UseStorageHook = {
-  setItem: <T = any>(key: string, value: T) => T;
-  getItem: <T = any>(key: string, defaultValue?: T) => T;
-  popItem: <T = any>(key: string) => T;
-  removeItem: (key: string) => void;
-};
-
 export const useStorage = (storage: Storage = "local"): UseStorageHook => {
-  const setItem = <T = any>(key: string, value: T): T => {
-    isBrowser() ? window[getStorageType(storage)].setItem(key, JSON.stringify(value)) : undefined;
-
-    return value;
-  };
-
-  const getItem = <T = any>(key: string, defaultValue?: T): T => {
+  const getItem = <T = any>(key: string): T | undefined => {
     const value = isBrowser() ? window[getStorageType(storage)][key] : undefined;
-
-    return value ? JSON.parse(value) : defaultValue;
-  };
-
-  const popItem = <T = any>(key: string): T => {
-    const value = getItem(key);
-
-    if (value) {
-      removeItem(key);
-    }
-
-    return value;
+    return value ? JSON.parse(value) : undefined;
   };
 
   const removeItem = (key: string): void =>
     isBrowser() ? window[getStorageType(storage)].removeItem(key) : undefined;
 
+  const setItem = <T = any>(key: string, value: T): void =>
+    isBrowser() ? window[getStorageType(storage)].setItem(key, JSON.stringify(value)) : undefined;
+
   return {
-    setItem,
     getItem,
-    popItem,
     removeItem,
+    setItem,
   };
 };
