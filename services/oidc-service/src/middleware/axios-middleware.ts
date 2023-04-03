d1f0def2012e7dcfe305fc60db9683271a663c55
Jonn Nilsson <jonn@jonn.nu>
2023-04-03 22:59:15 +0200

    fix: amend bug

--- services/oidc-service/src/middleware/axios-middleware.ts
+++ services/oidc-service/src/middleware/axios-middleware.ts
@@ -1,6 +1,5 @@
 import { axiosClientCredentialsMiddleware } from "@lindorm-io/axios";
 import { configuration } from "../server/configuration";
-import { Environment } from "@lindorm-io/common-types";
 import { logger } from "../server/logger";
 
 export const clientCredentialsMiddleware = axiosClientCredentialsMiddleware({
@@ -8,6 +7,5 @@ export const clientCredentialsMiddleware = axiosClientCredentialsMiddleware({
   port: configuration.services.oauth_service.port,
   clientId: configuration.oauth.client_id,
   clientSecret: configuration.oauth.client_secret,
-  environment: configuration.server.environment as Environment,
   logger,
 });
