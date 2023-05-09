b8bbceeed819063350ff9230ac3046fd5d547578
Jonn Nilsson <jonn@jonn.nu>
2023-05-09 10:35:15 +0200

    fix: add options to opaque token

--- packages/jwt/src/util/public/create-opaque-token.ts
+++ packages/jwt/src/util/public/create-opaque-token.ts
@@ -1,14 +1,18 @@
 import { Dict } from "@lindorm-io/common-types";
 import { baseHash } from "@lindorm-io/core";
-import { randomToken } from "@lindorm-io/random";
+import { RandomStringOptions, randomToken } from "@lindorm-io/random";
 import { TokenHeaderType } from "../../enum";
 
-export const createOpaqueToken = (length = 192, header: Dict = {}): string => {
+export const createOpaqueToken = (
+  length = 128,
+  header: Dict = {},
+  options: RandomStringOptions = {},
+): string => {
   const head = baseHash(JSON.stringify({ ...header, typ: TokenHeaderType.OPAQUE })).replace(
     /=/g,
     "",
   );
-  const payload = randomToken(length);
+  const payload = randomToken(length, options);
 
   return `${head}.${payload}`;
 };
