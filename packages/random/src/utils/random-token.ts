824532b3c9f4313ebd215fddcbb8a7f808e88adc
Jonn Nilsson <jonn@jonn.nu>
2023-05-09 10:35:03 +0200

    fix: add options to random token

--- packages/random/src/utils/random-token.ts
+++ packages/random/src/utils/random-token.ts
@@ -1,9 +1,11 @@
-import { randomString } from "./random-string";
 import { UNRESERVED_TOKEN_SYMBOLS } from "../constants";
+import { RandomStringOptions } from "../types";
+import { randomString } from "./random-string";
 
-export const randomToken = (length: number): string =>
+export const randomToken = (length: number, options: RandomStringOptions = {}): string =>
   randomString(length, {
     numbers: "random",
     symbols: "20%",
     custom: { symbols: UNRESERVED_TOKEN_SYMBOLS.join("") },
+    ...options,
   });
