336413b059d4db883d0449d17f0c4822d8d33862
Jonn Nilsson <jonn@jonn.nu>
2023-06-02 10:32:44 +0200

    fix: update type

--- packages/koa-jwt/src/types/custom-validation.ts
+++ packages/koa-jwt/src/types/custom-validation.ts
@@ -1,7 +1,7 @@
-import { JwtDecodeResult } from "@lindorm-io/jwt";
+import { JwtVerify } from "@lindorm-io/jwt";
 import { DefaultLindormKoaContext } from "@lindorm-io/koa";
 
 export type TokenCustomValidation<
   Context extends DefaultLindormKoaContext = DefaultLindormKoaContext,
   Claims = any,
-> = (ctx: Context, verifyData: JwtDecodeResult<Claims>) => Promise<void>;
+> = (ctx: Context, verifyData: JwtVerify<Claims>) => Promise<void>;
