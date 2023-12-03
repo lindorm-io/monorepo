8007c926c3f5ec9c75464e4346998628867e6c30
Jonn Nilsson <jonn@jonn.nu>
2023-12-03 12:24:49 +0100

    fix: update grant types

--- packages/common-enums/src/enums/OpenIdGrantType.ts
+++ packages/common-enums/src/enums/OpenIdGrantType.ts
@@ -1,9 +1,15 @@
 export enum OpenIdGrantType {
   AUTHENTICATION_TOKEN = "urn:ietf:params:oauth:grant-type:authentication-token",
-  AUTHORIZATION_CODE = "authorization_code",
-  CLIENT_CREDENTIALS = "client_credentials",
+  AUTHORIZATION_CODE = "urn:ietf:params:oauth:grant-type:authorization-code",
+  BACKCHANNEL_AUTHENTICATION = "urn:openid:params:grant-type:ciba",
+  CLIENT_CREDENTIALS = "urn:ietf:params:oauth:grant-type:client-credentials",
   JWT_BEARER = "urn:ietf:params:oauth:grant-type:jwt-bearer",
-  PASSWORD = "password",
-  REFRESH_TOKEN = "refresh_token",
+  PASSWORD = "urn:ietf:params:oauth:grant-type:password",
+  REFRESH_TOKEN = "urn:ietf:params:oauth:grant-type:refresh-token",
   TOKEN_EXCHANGE = "urn:ietf:params:oauth:grant-type:token-exchange",
+
+  OAUTH2_AUTHORIZATION_CODE = "authorization_code",
+  OAUTH2_CLIENT_CREDENTIALS = "client_credentials",
+  OAUTH2_PASSWORD = "password",
+  OAUTH2_REFRESH_TOKEN = "refresh_token",
 }
