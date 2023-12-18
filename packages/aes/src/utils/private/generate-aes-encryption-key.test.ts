f3037ef3b577bb1b1fdd46a2e2e17cc30bb55db3
Jonn Nilsson <jonn@jonn.nu>
2023-12-18 10:24:48 +0100

    fix: amend error in encryption key generation

--- packages/aes/src/utils/private/generate-aes-encryption-key.test.ts
+++ packages/aes/src/utils/private/generate-aes-encryption-key.test.ts
@@ -3,6 +3,18 @@ import { AesError } from "../../errors";
 import { generateAesEncryptionKey } from "./generate-aes-encryption-key";
 
 describe("generateAesEncryptionKey", () => {
+  test("should resolve for aes-128-cbc", () => {
+    expect(generateAesEncryptionKey(AesAlgorithm.AES_128_CBC)).toHaveLength(16);
+  });
+
+  test("should resolve for aes-192-cbc", () => {
+    expect(generateAesEncryptionKey(AesAlgorithm.AES_192_CBC)).toHaveLength(24);
+  });
+
+  test("should resolve for aes-256-cbc", () => {
+    expect(generateAesEncryptionKey(AesAlgorithm.AES_256_CBC)).toHaveLength(32);
+  });
+
   test("should resolve for aes-128-gcm", () => {
     expect(generateAesEncryptionKey(AesAlgorithm.AES_128_GCM)).toHaveLength(16);
   });
--- packages/aes/src/utils/private/generate-aes-encryption-key.ts
+++ packages/aes/src/utils/private/generate-aes-encryption-key.ts
@@ -5,6 +5,15 @@ import { AesCipherAlgorithm } from "../../types";
 
 export const generateAesEncryptionKey = (algorithm: AesCipherAlgorithm): Buffer => {
   switch (algorithm) {
+    case AesAlgorithm.AES_128_CBC:
+      return randomBytes(16);
+
+    case AesAlgorithm.AES_192_CBC:
+      return randomBytes(24);
+
+    case AesAlgorithm.AES_256_CBC:
+      return randomBytes(32);
+
     case AesAlgorithm.AES_128_GCM:
       return randomBytes(16);
 
