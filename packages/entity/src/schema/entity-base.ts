bb2836af6159880f404ab963fd109bab288c1315
Jonn Nilsson <jonn@jonn.nu>
2024-01-12 10:50:01 +0100

    fix: update schema

--- packages/entity/src/schema/entity-base.ts
+++ packages/entity/src/schema/entity-base.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const JOI_ENTITY_BASE = {
-  id: Joi.string().guid({ version: "uuidv4", separator: "-" }).required(),
+  id: Joi.string().guid().required(),
   created: Joi.date().iso().required(),
   revision: Joi.number().integer().min(0).required(),
   updated: Joi.date().iso().required(),
