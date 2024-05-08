bb6ae8fb16d2e92e9a18932cc9e4042967d6d852
Jonn Nilsson <jonn@jonn.nu>
2024-05-08 09:32:58 +0200

    feat: initialise types package

---
+++ packages/types/src/types/types.ts
@@ -0,0 +1,13 @@
+export type ClassLike = { constructor: { name: string } };
+
+export type Constructor<T> = new (...args: any) => T;
+
+export type Dict<T = any> = Record<string, T>;
+
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type Param = string | number | boolean | Array<string | number | boolean>;
+
+export type Query = string | number | boolean | Array<string | number | boolean>;
+
+export type ReverseMap<T> = T[keyof T];
