a5e8239b6223d21ea44f6703ae12ab7089341fe5
Jonn Nilsson <jonn@jonn.nu>
2024-05-10 00:07:52 +0200

    fix: allow strings for plain url

--- packages/url/src/utils/get-plain-url.ts
+++ packages/url/src/utils/get-plain-url.ts
@@ -1,5 +1,7 @@
-export const getPlainUrl = (url: URL): URL => {
-  const { origin, pathname } = url;
+import { isUrl } from "@lindorm/is";
+
+export const getPlainUrl = (url: URL | string): URL => {
+  const { origin, pathname } = isUrl(url) ? url : new URL(url);
 
   return new URL(pathname, origin);
 };
