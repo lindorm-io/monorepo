ba98ec12d4733fa36252a3486503d26d5d5a1088
Jonn Nilsson <jonn@jonn.nu>
2024-05-09 20:30:46 +0200

    fix: add header type

--- packages/types/src/types/types.ts
+++ packages/types/src/types/types.ts
@@ -6,6 +6,8 @@ export type Dict<T = any> = Record<string, T>;
 
 export type Function<T = any> = (...args: any[]) => T;
 
+export type Header = string | number | boolean;
+
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
 export type Param = string | number | boolean | Array<string | number | boolean>;
