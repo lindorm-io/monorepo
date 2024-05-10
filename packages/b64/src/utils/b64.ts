d9ae175a4b9104f3e8ba0a79257982b423a3e71c
Jonn Nilsson <jonn@jonn.nu>
2024-05-10 22:59:41 +0200

    fix: improve decoding

--- packages/b64/src/utils/b64.ts
+++ packages/b64/src/utils/b64.ts
@@ -3,7 +3,8 @@ import { _decode } from "./private/decode";
 import { _encodeBase64, _encodeBase64Url } from "./private/encode";
 
 export const B64 = {
-  encode: (input: string, encoding: Base64Encoding = "base64"): string =>
+  buffer: (input: string): Buffer => _decode(input),
+  decode: (input: string): string => _decode(input).toString(),
+  encode: (input: Buffer | string, encoding: Base64Encoding = "base64"): string =>
     encoding === "base64" ? _encodeBase64(input) : _encodeBase64Url(input),
-  decode: _decode,
 };
--- packages/b64/src/utils/private/encode.ts
+++ packages/b64/src/utils/private/encode.ts
@@ -1,12 +1,18 @@
 const toBase64Url = (base64: string): string =>
   base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
 
-export const _encodeBase64 = (input: string): string => Buffer.from(input).toString("base64");
+export const _encodeBase64 = (input: Buffer | string, encoding: BufferEncoding = "utf8"): string =>
+  Buffer.isBuffer(input)
+    ? input.toString("base64")
+    : Buffer.from(input, encoding).toString("base64");
 
-export const _encodeBase64Url = (input: any, encoding: BufferEncoding = "utf8"): string => {
-  if (Buffer.isEncoding("base64url")) {
-    return Buffer.from(input, encoding).toString("base64url");
-  } else {
-    return toBase64Url(Buffer.from(input, encoding).toString("base64"));
-  }
+export const _encodeBase64Url = (
+  input: Buffer | string,
+  encoding: BufferEncoding = "utf8",
+): string => {
+  const buffer = Buffer.isBuffer(input) ? input : Buffer.from(input, encoding);
+
+  return Buffer.isEncoding("base64url")
+    ? buffer.toString("base64url")
+    : toBase64Url(buffer.toString("base64"));
 };
