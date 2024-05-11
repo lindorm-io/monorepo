614eef99ded60850e546fd1672ac55616821632b
Jonn Nilsson <jonn@jonn.nu>
2024-05-11 14:21:56 +0200

    fix: add max attempts to config

--- packages/retry/src/types/retry.ts
+++ packages/retry/src/types/retry.ts
@@ -2,9 +2,13 @@ import { Optional } from "@lindorm/types";
 import { RetryStrategy } from "../enums";
 
 export type RetryConfig = {
+  maxAttempts: number;
   strategy: RetryStrategy;
   timeout: number;
   timeoutMax: number;
 };
 
-export type RetryOptions = Optional<RetryConfig, "strategy" | "timeout" | "timeoutMax">;
+export type RetryOptions = Optional<
+  RetryConfig,
+  "maxAttempts" | "strategy" | "timeout" | "timeoutMax"
+>;
