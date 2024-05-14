ba801c6c396794bc44228b0ef4d9f432f320f452
Jonn Nilsson <jonn@jonn.nu>
2024-05-14 23:15:15 +0200

    fix: improve b64 decoding slightly

--- packages/b64/src/utils/b64.ts
+++ packages/b64/src/utils/b64.ts
@@ -2,9 +2,16 @@ import { Base64Encoding } from "../types";
 import { _decode } from "./private/decode";
 import { _encodeBase64, _encodeBase64Url } from "./private/encode";
 
-export const B64 = {
-  buffer: (input: string): Buffer => _decode(input),
-  decode: (input: string): string => _decode(input).toString(),
-  encode: (input: Buffer | string, encoding: Base64Encoding = "base64"): string =>
-    encoding === "base64" ? _encodeBase64(input) : _encodeBase64Url(input),
-};
+export class B64 {
+  public static buffer(input: string, encoding?: Base64Encoding): Buffer {
+    return _decode(input, encoding);
+  }
+
+  public static decode(input: string, encoding?: Base64Encoding): string {
+    return _decode(input, encoding).toString();
+  }
+
+  public static encode(input: Buffer | string, encoding: Base64Encoding = "base64"): string {
+    return encoding === "base64" ? _encodeBase64(input) : _encodeBase64Url(input);
+  }
+}
--- packages/b64/src/utils/private/decode.ts
+++ packages/b64/src/utils/private/decode.ts
@@ -1,4 +1,10 @@
-export const _decode = (input: string): Buffer => {
+import { Base64Encoding } from "../../types";
+
+export const _decode = (input: string, encoding?: Base64Encoding): Buffer => {
+  if (encoding === "base64") {
+    return Buffer.from(input, "base64");
+  }
+
   const base64 = input.replace(/-/g, "+").replace(/_/g, "/");
 
   const padLength = (4 - (base64.length % 4)) % 4;
