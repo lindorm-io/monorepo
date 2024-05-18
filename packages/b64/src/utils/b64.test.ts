0f6a263843e21948ba83081ba1bdd2b5f2d55c5a
Jonn Nilsson <jonn@jonn.nu>
2024-05-18 23:56:25 +0200

    fix: improve method nomenclature

--- packages/b64/src/utils/b64.test.ts
+++ packages/b64/src/utils/b64.test.ts
@@ -1,27 +1,45 @@
 import { B64 } from "./b64";
 
 describe("B64", () => {
-  test("should encode base64", () => {
-    expect(B64.encode("hello there - general kenobi")).toEqual(
-      "aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ==",
-    );
+  describe("encode", () => {
+    test("should encode base64", () => {
+      expect(B64.encode("hello there - general kenobi")).toEqual(
+        "aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ==",
+      );
+    });
+
+    test("should encode base64Url", () => {
+      expect(B64.encode("hello there - general kenobi", "base64url")).toEqual(
+        "aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ",
+      );
+    });
   });
 
-  test("should encode base64Url", () => {
-    expect(B64.encode("hello there - general kenobi", "base64url")).toEqual(
-      "aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ",
-    );
+  describe("decode to buffer", () => {
+    test("should decode base64", () => {
+      expect(B64.toBuffer("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ==")).toEqual(
+        Buffer.from("hello there - general kenobi"),
+      );
+    });
+
+    test("should decode base64Url", () => {
+      expect(B64.toBuffer("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ")).toEqual(
+        Buffer.from("hello there - general kenobi"),
+      );
+    });
   });
 
-  test("should decode base64", () => {
-    expect(B64.decode("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ==")).toEqual(
-      "hello there - general kenobi",
-    );
-  });
+  describe("decode to string", () => {
+    test("should decode base64", () => {
+      expect(B64.toString("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ==")).toEqual(
+        "hello there - general kenobi",
+      );
+    });
 
-  test("should decode base64Url", () => {
-    expect(B64.decode("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ")).toEqual(
-      "hello there - general kenobi",
-    );
+    test("should decode base64Url", () => {
+      expect(B64.toString("aGVsbG8gdGhlcmUgLSBnZW5lcmFsIGtlbm9iaQ")).toEqual(
+        "hello there - general kenobi",
+      );
+    });
   });
 });
--- packages/b64/src/utils/b64.ts
+++ packages/b64/src/utils/b64.ts
@@ -3,15 +3,18 @@ import { _decode } from "./private/decode";
 import { _encodeBase64, _encodeBase64Url } from "./private/encode";
 
 export class B64 {
-  public static buffer(input: string, encoding?: Base64Encoding): Buffer {
+  public static encode(
+    input: Buffer | string,
+    encoding: Base64Encoding = "base64",
+  ): string {
+    return encoding === "base64" ? _encodeBase64(input) : _encodeBase64Url(input);
+  }
+
+  public static toBuffer(input: string, encoding?: Base64Encoding): Buffer {
     return _decode(input, encoding);
   }
 
-  public static decode(input: string, encoding?: Base64Encoding): string {
+  public static toString(input: string, encoding?: Base64Encoding): string {
     return _decode(input, encoding).toString();
-  }
-
-  public static encode(input: Buffer | string, encoding: Base64Encoding = "base64"): string {
-    return encoding === "base64" ? _encodeBase64(input) : _encodeBase64Url(input);
   }
 }
