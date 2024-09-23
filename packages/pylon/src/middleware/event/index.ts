0f15e832ad08b1db997b8046cee56a18ef740f12
Jonn Nilsson <jonn@jonn.nu>
2024-09-23 08:36:03 +0200

    build: rename files

--- packages/pylon/src/middleware/event/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./event-bearer-token-middleware";
-export * from "./event-conduit-middleware";
-export * from "./event-token-middleware";
--- packages/pylon/src/middleware/index.ts
+++ packages/pylon/src/middleware/index.ts
@@ -1,2 +1,2 @@
-export * from "./event";
+export * from "./socket";
 export * from "./http";
