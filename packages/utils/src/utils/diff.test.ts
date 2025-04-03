d3440f83464768e0d62e853fce6507e2e21fbb62
Jonn Nilsson <jonn@jonn.nu>
2025-04-03 13:33:03 +0200

    fix: return array from diff and not boolean

--- packages/utils/src/utils/diff.test.ts
+++ packages/utils/src/utils/diff.test.ts
@@ -1,11 +1,17 @@
-import { diff } from "./diff";
+import { diff, diffAny } from "./diff";
 
 describe("diff", () => {
-  test("should return true if all elements are in source", () => {
-    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5])).toEqual(true);
+  test("should return an empty array if all elements are in source", () => {
+    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5])).toEqual([]);
   });
 
-  test("should return false if not all elements are in source", () => {
-    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual(false);
+  test("should return array of items if not all elements are in source", () => {
+    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([10]);
+  });
+
+  test("should return array of all items not equal in source or target", () => {
+    expect(diffAny([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([
+      10, 6, 7, 8, 9,
+    ]);
   });
 });
--- packages/utils/src/utils/diff.ts
+++ packages/utils/src/utils/diff.ts
@@ -1,4 +1,12 @@
-export const diff = (source: Array<any>, target: Array<any>): boolean => {
-  const setA = new Set(source);
-  return target.every((element) => setA.has(element));
+export const diff = <T extends Array<any> = Array<any>>(
+  source: Array<any>,
+  target: T,
+): T => {
+  const set = new Set(source);
+  return target.filter((element) => !set.has(element)) as T;
 };
+
+export const diffAny = <T extends Array<any> = Array<any>>(
+  source: Array<any>,
+  target: T,
+): T => [...diff(source, target), ...diff(target, source)] as T;
