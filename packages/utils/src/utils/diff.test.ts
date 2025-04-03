184621fdbde1357abdbd7039eda90b0abf6cb96b
Jonn Nilsson <jonn@jonn.nu>
2025-04-03 13:35:58 +0200

    fix: allow for complex diff arrays

--- packages/utils/src/utils/diff.test.ts
+++ packages/utils/src/utils/diff.test.ts
@@ -9,6 +9,12 @@ describe("diff", () => {
     expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([10]);
   });
 
+  test("should return array of items in complex object array", () => {
+    expect(
+      diff([{ test: 1 }, { test: 2 }, { test: 3 }], [{ test: 1 }, { test: 4 }]),
+    ).toEqual([{ test: 4 }]);
+  });
+
   test("should return array of all items not equal in source or target", () => {
     expect(diffAny([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([
       10, 6, 7, 8, 9,
--- packages/utils/src/utils/diff.ts
+++ packages/utils/src/utils/diff.ts
@@ -1,10 +1,9 @@
+import { isEqual } from "@lindorm/is";
+
 export const diff = <T extends Array<any> = Array<any>>(
   source: Array<any>,
   target: T,
-): T => {
-  const set = new Set(source);
-  return target.filter((element) => !set.has(element)) as T;
-};
+): T => target.filter((t) => !source.some((s) => isEqual(s, t))) as T;
 
 export const diffAny = <T extends Array<any> = Array<any>>(
   source: Array<any>,
