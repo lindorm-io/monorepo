d9d2802d9d3f902d38ac814077e390116d0c8198
Jonn Nilsson <jonn@jonn.nu>
2025-05-06 10:50:51 +0200

    build: improve readable time typing

--- packages/date/src/types/readable-time.ts
+++ packages/date/src/types/readable-time.ts
@@ -1,35 +1,43 @@
-type UnitLong =
-  | "Years"
-  | "Year"
-  | "Yrs"
-  | "Yr"
-  | "Months"
-  | "Month"
-  | "Weeks"
-  | "Week"
-  | "Days"
-  | "Day"
-  | "Hours"
-  | "Hour"
-  | "Hrs"
-  | "Hr"
-  | "Minutes"
-  | "Minute"
-  | "Mins"
-  | "Min"
-  | "Seconds"
-  | "Second"
-  | "Secs"
-  | "Sec"
-  | "Milliseconds"
-  | "Millisecond"
-  | "Msecs"
-  | "Msec";
+export const UNIT_LONG = [
+  "Years",
+  "Year",
+  "Yrs",
+  "Yr",
+  "Months",
+  "Month",
+  "Weeks",
+  "Week",
+  "Days",
+  "Day",
+  "Hours",
+  "Hour",
+  "Hrs",
+  "Hr",
+  "Minutes",
+  "Minute",
+  "Mins",
+  "Min",
+  "Seconds",
+  "Second",
+  "Secs",
+  "Sec",
+  "Milliseconds",
+  "Millisecond",
+  "Msecs",
+  "Msec",
+] as const;
 
-type UnitShort = "Y" | "Mo" | "W" | "D" | "H" | "M" | "S" | "Ms";
+export const UNIT_SHORT = ["Y", "Mo", "W", "D", "H", "M", "S", "Ms"] as const;
 
-type Unit = UnitLong | UnitShort;
+export const UNIT_ANY_CASE = [
+  ...UNIT_LONG,
+  ...UNIT_SHORT,
+  ...UNIT_LONG.map((unit) => unit.toUpperCase()),
+  ...UNIT_LONG.map((unit) => unit.toLowerCase()),
+  ...UNIT_SHORT.map((unit) => unit.toUpperCase()),
+  ...UNIT_SHORT.map((unit) => unit.toLowerCase()),
+] as const;
 
-type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>;
+export type ReadableUnit = (typeof UNIT_ANY_CASE)[number];
 
-export type ReadableTime = `${number}${UnitAnyCase}` | `${number} ${UnitAnyCase}`;
+export type ReadableTime = `${number}${ReadableUnit}` | `${number} ${ReadableUnit}`;
