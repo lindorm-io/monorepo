8f924c9ac7474f7d9027d5b2fd33af6ba7bc4eb2
Jonn Nilsson <jonn@jonn.nu>
2025-05-06 10:49:04 +0200

    fix: sort deep keys

--- packages/utils/src/utils/sort-keys.test.ts
+++ packages/utils/src/utils/sort-keys.test.ts
@@ -2,6 +2,24 @@ import { sortKeys } from "./sort-keys";
 
 describe("sortKeys", () => {
   test("should sort object keys alphabetically", () => {
-    expect(sortKeys({ e: 1, b: 1, z: 1, a: 2 })).toEqual({ a: 2, b: 1, e: 1, z: 1 });
+    expect(
+      sortKeys({
+        e: 1,
+        b: 1,
+        d: {
+          k: 1,
+          u: 1,
+          a: 1,
+          c: 1,
+          f: {
+            i: 1,
+            h: 1,
+            g: 1,
+          },
+        },
+        z: 1,
+        a: 2,
+      }),
+    ).toMatchSnapshot();
   });
 });
--- packages/utils/src/utils/sort-keys.ts
+++ packages/utils/src/utils/sort-keys.ts
@@ -1,10 +1,15 @@
+import { isObjectLike } from "@lindorm/is";
 import { Dict } from "@lindorm/types";
 
 export function sortKeys<T extends Dict = Dict>(arg: T): T {
   const result: Dict = {};
 
-  for (const key of Object.keys(arg as Dict).sort()) {
-    result[key] = (arg as Dict)[key];
+  for (const [key, value] of Object.entries(arg).sort()) {
+    if (isObjectLike(value)) {
+      result[key] = sortKeys(value);
+    } else {
+      result[key] = value;
+    }
   }
 
   return result as T;
