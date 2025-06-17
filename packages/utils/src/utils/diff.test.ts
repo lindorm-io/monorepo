d875253c2c0e9ca6f77af245cc2543a3a58669c2
Jonn Nilsson <jonn@jonn.nu>
2025-06-17 09:41:38 +0200

    feat: merge array and dict diff functions

--- packages/utils/src/utils/diff.test.ts
+++ packages/utils/src/utils/diff.test.ts
@@ -1,17 +1,17 @@
-import { diff, diffAny, diffObject } from "./diff";
+import { diffAny, diffArray, diffObject } from "./diff";
 
 describe("diff", () => {
   test("should return an empty array if all elements are in source", () => {
-    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5])).toEqual([]);
+    expect(diffArray([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5])).toEqual([]);
   });
 
   test("should return array of items if not all elements are in source", () => {
-    expect(diff([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([10]);
+    expect(diffArray([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 10])).toEqual([10]);
   });
 
   test("should return array of items in complex object array", () => {
     expect(
-      diff([{ test: 1 }, { test: 2 }, { test: 3 }], [{ test: 1 }, { test: 4 }]),
+      diffArray([{ test: 1 }, { test: 2 }, { test: 3 }], [{ test: 1 }, { test: 4 }]),
     ).toEqual([{ test: 4 }]);
   });
 });
--- packages/utils/src/utils/diff.ts
+++ packages/utils/src/utils/diff.ts
@@ -1,15 +1,10 @@
-import { isEqual, isObject } from "@lindorm/is";
+import { isArray, isEqual, isObject } from "@lindorm/is";
 import { DeepPartial, Dict } from "@lindorm/types";
 
-export const diff = <T extends Array<any> = Array<any>>(
+export const diffArray = <T extends Array<any> = Array<any>>(
   source: Array<any>,
   target: T,
 ): T => target.filter((t) => !source.some((s) => isEqual(s, t))) as T;
-
-export const diffAny = <T extends Array<any> = Array<any>>(
-  source: Array<any>,
-  target: T,
-): T => [...diff(source, target), ...diff(target, source)] as T;
 
 export const diffObject = <T extends Dict = Dict>(
   source: Dict,
@@ -58,3 +53,23 @@ export const diffObject = <T extends Dict = Dict>(
 
   return patch;
 };
+
+export function diff<T extends Dict>(source: Dict, target: T): DeepPartial<T>;
+export function diff<T extends Array<any>>(source: Array<any>, target: T): T;
+export function diff<T extends Dict | Array<any>>(
+  source: Dict | Array<any>,
+  target: T,
+): DeepPartial<T> {
+  if (isArray(source) && isArray(target)) {
+    return diffArray(source, target) as DeepPartial<T>;
+  }
+  if (isObject(source) && isObject(target)) {
+    return diffObject(source as Dict, target as DeepPartial<T>) as DeepPartial<T>;
+  }
+  throw new TypeError("Source and target must be of the same type (array or object)");
+}
+
+export const diffAny = <T extends Array<any> = Array<any>>(
+  source: Array<any>,
+  target: T,
+): T => [...diff(source, target), ...diff(target, source)] as T;
