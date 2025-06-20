016e68fcd63aa5278c5ea2ef4f882100ea7b0a40
Jonn Nilsson <jonn@jonn.nu>
2025-06-20 15:41:35 +0200

    fix: add common priority enum

---
+++ packages/enums/src/enums/Priority.ts
@@ -0,0 +1,10 @@
+export enum Priority {
+  Default = "urn:lindorm:priority:default",
+
+  Critical = "urn:lindorm:priority:critical",
+  High = "urn:lindorm:priority:high",
+  Medium = "urn:lindorm:priority:medium",
+  Low = "urn:lindorm:priority:low",
+
+  Background = "urn:lindorm:priority:background",
+}
--- packages/enums/src/enums/index.ts
+++ packages/enums/src/enums/index.ts
@@ -1,3 +1,4 @@
 export * from "./Environment";
 export * from "./HttpMethod";
 export * from "./PkceMethod";
+export * from "./Priority";
