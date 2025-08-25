c178040325e9f309c20766b04cf71adc6f750753
Jonn Nilsson <jonn@jonn.nu>
2025-08-25 11:34:54 +0200

    fix: add utilities for true and false

--- packages/is/src/utils/is-boolean.ts
+++ packages/is/src/utils/is-boolean.ts
@@ -1,1 +1,5 @@
 export const isBoolean = (input?: any): input is boolean => typeof input === "boolean";
+
+export const isTrue = (input?: any): input is true => input === true;
+
+export const isFalse = (input?: any): input is false => input === false;
