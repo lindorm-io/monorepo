359f139e9de495c4fbc61ee26f40b07db5560016
Jonn Nilsson <jonn@jonn.nu>
2025-09-18 08:50:31 +0200

    build: update pylon types

--- packages/pylon/src/utils/private/compose-parse-body-config.ts
+++ packages/pylon/src/utils/private/compose-parse-body-config.ts
@@ -1,17 +1,16 @@
 import { HttpMethod } from "@lindorm/types";
 import { ParseBodyConfig, ParseBodyOptions } from "../../types";
 
-export const composeParseBodyConfig = (options?: ParseBodyOptions): ParseBodyConfig => {
-  const { formidableOptions = {} } = options ?? {};
+export const composeParseBodyConfig = (
+  options: ParseBodyOptions = {},
+): ParseBodyConfig => {
+  const { formidableOptions = {} } = options;
 
-  const { json = "1mb", form = "1mb", text = "1mb" } = options?.limits ?? {};
+  const { json = "1mb", form = "1mb", text = "1mb" } = options.limits ?? {};
 
-  const formidable =
-    options?.formidable ?? options?.formidableOptions !== undefined ?? false;
-
-  const methods = (options?.methods as Array<HttpMethod>) ?? ["POST", "PUT", "PATCH"];
-
-  const multipart = options?.multipart ?? formidable ?? false;
+  const formidable = options.formidable ?? options.formidableOptions !== undefined;
+  const methods = (options.methods as Array<HttpMethod>) ?? ["POST", "PUT", "PATCH"];
+  const multipart = options.multipart ?? formidable ?? false;
 
   return {
     formidable,
