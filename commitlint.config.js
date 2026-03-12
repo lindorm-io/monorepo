ab152e678b6e7a6b4ff6a291b992d789033f97c1
Jonn Nilsson <jonn@jonn.nu>
2026-03-12 10:17:06 +0100

    build: update commitlint config with custom rules and add __tests__ build exclusion

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [2, "always", 120],
+    "footer-max-line-length": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "wip",
+      ],
+    ],
+  },
 };
