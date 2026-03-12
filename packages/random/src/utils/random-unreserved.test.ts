affcbb88bf4ba95bb100f9e54c4bc297f2b547a7
Jonn Nilsson <jonn@jonn.nu>
2026-03-12 10:23:44 +0100

    refactor(random): rewrite with crypto.getRandomValues and simplify API

--- packages/random/src/utils/random-unreserved.test.ts
+++
@@ -1,7 +0,0 @@
-import { randomUnreserved } from "./random-unreserved";
-
-describe("randomUnreserved", () => {
-  test("should return a random unreserved", () => {
-    expect(randomUnreserved(10)).toEqual(expect.any(String));
-  });
-});
