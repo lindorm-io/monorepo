6731ff306aa611a6c05aa3d1f63520c12e1ec06f
Jonn Nilsson <jonn@jonn.nu>
2026-03-28 13:07:44 +0100

    chore: remove deprecated kafka package
    Superseded by @lindorm/iris unified messaging package.

--- packages/kafka/src/interfaces/KafkaDelayStore.ts
+++
@@ -1,8 +0,0 @@
-import { KafkaDelayOptions } from "../types";
-import { IKafkaDelayEnvelope } from "./KafkaDelayEnvelope";
-
-export interface IKafkaDelayStore {
-  get(topic: string): Promise<Array<IKafkaDelayEnvelope>>;
-  add(envelope: KafkaDelayOptions): Promise<void>;
-  ack(id: string): Promise<void>;
-}
