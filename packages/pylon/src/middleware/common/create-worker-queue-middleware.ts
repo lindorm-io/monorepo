5e73e182be02384a831c4426470eacdb190bfa68
Jonn Nilsson <jonn@jonn.nu>
2026-04-05 15:38:34 +0200

    refactor(pylon): use lazyFactory in convenience middleware
    Replace hand-rolled Object.defineProperty lazy getters with lazyFactory
    from @lindorm/utils in repository, cache, publisher, and worker queue
    middleware.

--- packages/pylon/src/middleware/common/create-worker-queue-middleware.ts
+++ packages/pylon/src/middleware/common/create-worker-queue-middleware.ts
@@ -2,6 +2,7 @@ import { resolveIris } from "#internal/utils/resolve-iris";
 import { camelCase } from "@lindorm/case";
 import { IIrisSource, IMessage } from "@lindorm/iris";
 import { Constructor, Dict } from "@lindorm/types";
+import { lazyFactory } from "@lindorm/utils";
 import { PylonContext, PylonMiddleware } from "../../types";
 
 export const createWorkerQueueMiddleware = <C extends PylonContext = PylonContext>(
@@ -13,12 +14,7 @@ export const createWorkerQueueMiddleware = <C extends PylonContext = PylonContex
     const obj: Dict = {};
 
     for (const message of messages) {
-      let cached: any;
-      Object.defineProperty(obj, camelCase(message.name), {
-        get: () => (cached ??= source.workerQueue(message)),
-        enumerable: true,
-        configurable: true,
-      });
+      lazyFactory(obj, camelCase(message.name), () => source.workerQueue(message));
     }
 
     ctx.workerQueues = obj;
