04e9808af018c875244424ef3c217529b52add4f
Jonn Nilsson <jonn@jonn.nu>
2026-04-13 12:38:41 +0200

    refactor(is): add generic type parameter to isNumber and isFinite

--- packages/is/src/utils/is-finite.ts
+++ packages/is/src/utils/is-finite.ts
@@ -1,4 +1,4 @@
 import { isNumber } from "./is-number";
 
-export const isFinite = (input?: any): input is number =>
+export const isFinite = <T extends number>(input?: any): input is T =>
   isNumber(input) && Number.isFinite(input);
