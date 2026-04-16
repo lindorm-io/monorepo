acec997179af987bd2700574c48339409246edfc
Jonn Nilsson <jonn@jonn.nu>
2026-04-16 15:18:38 +0200

    refactor(b64): migrate utils/private to #internal imports

--- packages/b64/src/classes/B64.ts
+++ packages/b64/src/classes/B64.ts
@@ -1,5 +1,5 @@
 import { Base64Encoding } from "../types";
-import { decode, encodeBase64, encodeBase64Url } from "../utils/private";
+import { decode, encodeBase64, encodeBase64Url } from "#internal/index";
 
 export class B64 {
   public static encode(
