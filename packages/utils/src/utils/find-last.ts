ee6d51b0b147879e766cec9e3e72a9fc38248041
Jonn Nilsson <jonn@jonn.nu>
2026-04-17 12:32:19 +0200

    refactor(utils): migrate #internal/* imports to relative paths
    Replace Node subpath imports with relative paths in source.
    Remove #internal/* mapping from tsconfig paths and package.json imports.

--- packages/utils/src/utils/find-last.ts
+++ packages/utils/src/utils/find-last.ts
@@ -1,5 +1,5 @@
 import { DeepPartial } from "@lindorm/types";
-import { matches } from "#internal/index";
+import { matches } from "../internal/index";
 
 export const findLast = <T>(array: Array<T>, partial: DeepPartial<T>): T | undefined =>
   array.filter((item) => matches(item, partial)).pop();
